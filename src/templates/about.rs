c0f1691ad32450583b0c9858c2b26448b86dc29d
talves <tony@3alves.com>
2022-11-02 16:10:03 -0700

    init: 🔥

---
+++ src/templates/about.rs
@@ -0,0 +1,21 @@
+use perseus::Template;
+use sycamore::prelude::{view, Html, SsrNode, View};
+
+#[perseus::template_rx]
+pub fn about_page() -> View<G> {
+    view! {
+        p { "About this app." }
+        a(href = "index", id = "home-link") { "Home!" }
+    }
+}
+
+#[perseus::head]
+pub fn head() -> View<SsrNode> {
+    view! {
+        title { "About Page | Perseus Example – Basic" }
+    }
+}
+
+pub fn get_template<G: Html>() -> Template<G> {
+    Template::new("about").template(about_page).head(head)
+}
